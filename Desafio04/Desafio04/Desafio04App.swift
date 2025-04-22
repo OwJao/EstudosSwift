a6f6dcae16ba4f1532e7b94a1c74b37b0a4d2a42
João Antônio Assunção <166741802+OwJao@users.noreply.github.com>
2025-04-22 20:56:14 -0300

    Add files via upload

---
+++ Desafio04/Desafio04/Desafio04App.swift
@@ -0,0 +1,17 @@
+//
+//  Desafio04App.swift
+//  Desafio04
+//
+//  Created by Turma02-28 on 22/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Desafio04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
